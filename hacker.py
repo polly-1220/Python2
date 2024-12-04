1f0ff2e14e2ade21f074acf431ae03670a931541
polly-1220 <bondarenko.p@academy.com.ua>
2024-12-04 16:58:13 +0200

    Update hacker.py

--- hacker.py
+++ hacker.py
@@ -1,11 +1,12 @@
 #login = input("Enter your email:")
 #password = input("Enter your password:")
 right_login = "user228"
-right_password = 15 
+right_password = 125 # з двох цифр
 
-for t in range(10):
-    for f in range(10):
-        pas = t*10 + f
+for h in range(10):
+  for t in range(10):
+     for f in range(10):
+        pas = h*100 t*10 + f
         print(pas)  
         if pas == right_password:
             print("ACCESS GRANTED! Pass: ", pas);
@@ -13,6 +14,8 @@ for t in range(10):
     if pas == right_password:
         print("ACCESS GRANTED! Pass: ", pas);
         break
+if pas == right_password:
+     break
 
 
 
