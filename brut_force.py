a2296c5978295c12619f955786fffe739cf5c2ba
Polina <bondarenko.p@academy.com.ua>
2025-02-03 15:03:56 +0200

    Update

--- brut_force.py
+++ brut_force.py
@@ -12,3 +12,18 @@ def brute_force(password_list):
             return password
      print("ACCED DENIED! Password is not found!") 
      return None      
+
+passwords =[
+    'qwerty123',
+    'Password1',
+    'admin2025',
+    'letmein!',
+    'welcome123',
+    'superSecure#2025',
+    '12345abcde',
+    'iloveyou@123',
+    'secretPassword',
+    'abc123!@#',  
+     ]
+     
+        
